e3161c25130bf080bf66c08d40456d2367802269
rorog2 <rorog2@live.fr>
2016-11-11 23:15:45 +0100

    Importation des transistor présents et des autre module et création des variables objet

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ import sys
 import os
 import getpass
 
+#Importation Autre
+from resistance import *
+from condensateur import *
 
 #Importation des transistors
 from transistor.t2n2222 import *
+from transistor.ttip120 import *
+from transistor.ttip31c import *
 
+#Création des variable d'objet
 T2n2222 = T2n2222()
+Ttip120 = Ttip120()
+Ttip31c = Ttip31c()
+
+#Code
 
 T2n2222.afficherCaracteristiques()
 
