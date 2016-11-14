1b991fb173484ce8e37ae2319efc2890b7814bf1
Romain GUITTEAU-GIROIRE <rguittea@pg.lyc>
2016-11-14 15:00:53 +0100

    Mise en place de la detection de la langue du systeme

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import getpass
+import locale
 
 #Importation Autre
 from resistance import *
@@ -16,6 +17,17 @@ from transistor.ttip31c import *
 T2n2222 = T2n2222()
 Ttip120 = Ttip120()
 Ttip31c = Ttip31c()
+
+#Initialisation
+
+#Langue du programme
+l1 = locale.getdefaultlocale()
+l2 = l1[0]
+langue = l2[0]+l2[1]
+if langue == "fr":
+    from texte.fr import *
+elif langue == "en":
+    from texte.en import *
 
 #Code
 
