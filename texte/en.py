790f93d0e7c0397164f123659051a55a33a39517
rorog2 <rorog2@live.fr>
2016-11-14 19:50:50 +0100

    Edition du fichier de langue "en"

--- texte/en.py
+++ texte/en.py
@@ -1,1 +1,25 @@
-﻿
+﻿demande = [
+"De quelle valeur de Ic votre montage ce raproche le plus ? ",#0
+"Quelle est la valeur de Ic dans votre montage? ",#1
+"Quelle valeur de beta correspond a votre montage ? ",#2
+"Quelle est la tension du générateur sur la base ? ",#3
+"Quelle est la tension du générateur sur le montage ? ",#4
+"Quelle est la tension de la diode ? "#5
+]
+
+reponse = [
+"Répondez par 1 ou 2: ",#0
+"Répondez par un chiffre entre 1 et 5: "#1
+]
+
+info = [
+"\nCARACTRISTIQUE DU TRANSISTOR {}",#0
+"Sa tension maximal est de {} V",#1
+"Son courant maximal est de {} A",#2
+"Ses tension de saturations entre le colecteur et l'éméteur sont:",#3
+"Sa tension de saturation entre le colecteur et l'éméteur est: {} V",#4
+"Ses tension de saturation entre l'éméteur et la base sont:",#5
+"Sa tension de saturation entre l'éméteur et la base est: {} V",#6
+"Ses coefficient beta sont:",#7
+"Son coefficient beta est: {}"#8
+]
