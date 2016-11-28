309912507699b68baae5727fa8f186b1839e8458
Romain GUITTEAU-GIROIRE <rguittea@pg.lyc>
2016-11-28 13:26:48 +0100

    Ajout des algorythme pour le pont diviseur

--- resistance.py
+++ resistance.py
@@ -1,5 +1,5 @@
 class Resistance:
-	
+
 	def __init__(self):
 		#VARIABLES
 		self.tension = 0.0
@@ -7,13 +7,21 @@ class Resistance:
 		self.courant = 0.0
 		self.anneau = 0.0
 		self.couleur = ""
-		
+        self.r1 = 0
+        self.r2 = 0
+
 	def calculResistance(self):
 		#DEMANDE D'INFORMATION
 		self.tension = float(input("Quelle est la tension dans votre montage ? "))
 		self.tension_composant = float(input("\nQuelle est la tension du composant a associer avec la resistance ? "))
 		self.courant = float(input("\nQuelle est le courant que doit recevoir votre composant ? "))
-		
+
 		#CALCUL ET RETOUR
 		self.resultat = (self.tension - self.tension_composant)/self.courant
 		return int(self.resultat)
+
+    def calculPontDiviseurVs(self):
+        #DEMANDE D'INFORMATION
+        self.tension = float(input("Quelle est la tension d'entrée ? "))
+        self.r1 = int(input("Quelle est la valeur de R1 ? "))
+        self.r2 = int(input("Quelle est la valeur de R2 ? "))
