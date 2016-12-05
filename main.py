67175eaf6365eeff33dec84d828d98244e62345e
Romain GUITTEAU-GIROIRE <rguittea@pg.lyc>
2016-12-05 14:26:20 +0100

    Changement divers de fichier et completition du main

--- main.py
+++ main.py
@@ -5,17 +5,30 @@ import sys
 import os
 import getpass
 import locale
+import time
+import platform
 
 #Importation Autre
 from resistance import *
 from condensateur import *
+from menu import *
 
 #Importation des transistors
 from transistor.t2n2222 import *
 from transistor.ttip120 import *
 from transistor.ttip31c import *
 
-#Creation des variable d'objet
+#--------Detection du systeme d'exploitation-----------
+#Detection du systeme
+systeme = platform.system()
+
+#Definition des commande
+if systeme == "Windows":
+    effacer = "cls"
+elif systeme == "Linux":
+    effacer = "clear"
+
+#-----------Creation des variable d'objet--------------
 #Divers
 Resistance = Resistance()
 Condensateur = Condensateur()
@@ -24,14 +37,71 @@ T2n2222 = T2n2222()
 Ttip120 = Ttip120()
 Ttip31c = Ttip31c()
 
-#Initialisation
+#------------------Initialisation----------------------
+menu = True     #Partie menu principal
+resistance = False      #Partie resistance
+condensateur = False        #Partie transistor
+transistor = False      #Partie condensateur
+quitter = False     #Quitter le programme
+
+#------------------------Code--------------------------
+bienvenue()
+
+while True:
+    try:
+        #Boucle du menu
+        if menu:
+            #Choix du menu
+            choix_menu_principal = menu_principal()
+            if choix_menu_principal == 1:
+                resistance = True
+            elif choix_menu_principal == 2:
+                transistor = True
+            elif choix_menu_principal == 3:
+                condensateur = True
+            elif choix_menu_principal == liste_menu_principal.__len__():
+                quitter = True
+
+            #Effacement du menu ou valeur fausse
+            if choix_menu_principal in liste_menu_principal:
+                menu = False
+            else:
+                os.system(effacer)
+                print("\nVotre choix n'est pas dans le menu !")
+                time.sleep(3)
+                os.system(effacer)
+
+        #Si resistance
+        if resistance:
+            #Choix du menu
+            choix_menu_resistance = menu_resistance()
+            if choix_menu_resistance == 1:
+                resultat = Resistance.calculResistance()
+                print("La résistance à appliquer est de: {}".format(resultat))
+            elif choix_menu_resistance == 2:
+                resultat = Resistance.calculPontDiviseurVs()
+                print("La tension de sortie est de: {}".format(resultat))
+            elif choix_menu_resistance == 3:
+                resultat = Resistance.calculPontDiviseurR2()
+                print("La résistance R2 à appliquer est de: {}".format(resultat))
+            elif choix_menu_resistance == liste_menu_resistance.__len__():
+                menu = True
+
+            #Effacement du menu ou valeur fausse
+            if choix_menu_principal in liste_menu_principal:
+                resistance = False
+            else:
+                os.system(effacer)
+                print("\nVotre choix n'est pas dans le menu !")
+                time.sleep(3)
+                os.system(effacer)
 
 
-
-#Code
-
-T2n2222.afficherCaracteristiques()
-#Ttip120.afficherCaracteristiques()
-#Ttip31c.afficherCaracteristiques()
+    #Si interruption du clavier
+    except KeyboardInterrupt:
+        os.system(effacer)
+        print("\nVous allez quitter la programme...")
+        time.sleep(3)
+        sys.exit(0)
 
 input("Appuyer sur une touche pour continuer...")
--- resistance.py
+++ resistance.py
@@ -1,31 +1,31 @@
 class Resistance:
 
-	def __init__(self):
-		#VARIABLES
-		self.tension = 0.0
-		self.tension_composant = 0.0
-		self.courant = 0.0
-		self.anneau = 0.0
-		self.couleur = ""
+    def __init__(self):
+        #VARIABLES
+        self.tension = 0.0
+        self.tension_composant = 0.0
+        self.courant = 0.0
+        self.anneau = 0.0
+        self.couleur = "."
         self.resultat = 0.0
         self.ve = 0.0
         self.vs = 0.0
         self.r1 = 0
         self.r2 = 0
 
-	def calculResistance(self):
-		#DEMANDE D'INFORMATION
-		self.tension = float(input("Quelle est la tension dans votre montage ? "))
-		self.tension_composant = float(input("\nQuelle est la tension du composant a associer avec la resistance ? "))
-		self.courant = float(input("\nQuelle est le courant que doit recevoir votre composant ? "))
+    def calculResistance(self):
+        #DEMANDE D'INFORMATION
+        self.tension = float(input("Quelle est la tension dans votre montage ? "))
+        self.tension_composant = float(input("\nQuelle est la tension du composant a associer avec la resistance ? "))
+        self.courant = float(input("\nQuelle est le courant que doit recevoir votre composant ? "))
 
 		#CALCUL ET RETOUR
-		self.resultat = (self.tension - self.tension_composant)/self.courant
-		return int(self.resultat)
+        self.resultat = (self.tension - self.tension_composant)/self.courant
+        return int(self.resultat)
 
     def calculPontDiviseurVs(self):
         #DEMANDE D'INFORMATION
-        self.ve = float(input("Quelle est la tension d'entrée ? "))
+        self.ve = float(input("Quelle est la tension d'entrÃ©e ? "))
         self.r1 = int(input("Quelle est la valeur de R1 ? "))
         self.r2 = int(input("Quelle est la valeur de R2 ? "))
 
@@ -42,3 +42,23 @@ class Resistance:
         #CALCUL ET RETOUR
         self.resultat = self.r1 * (self.ve / self.vs) - R1
         return self.resultat
+
+#Test du module seul
+if __name__ == "__main__":
+    #Menu
+    print("Que voulez-vous faire ?")
+    print("     1 - Calculer une résistance")
+    print("     2 - Calculer Vs dans un pont diviseur")
+    print("     3 - Calculer R2 dans un pont diviseur")
+    choix = int(input("\nQuelle est votre choix: "))
+    #Action suivant choix
+    if choix == 1:
+        res = Resistance().calculResistance()
+    elif choix == 2:
+        res = Resistance().calculPontDiviseurVs()
+    elif choix == 3:
+        res = Resistance().calculPontDiviseurR2()
+
+    #Affichage du résultat
+    print("Le résultat est: ", res)
+    input("Appuyer sur une touche pour continuer...")
